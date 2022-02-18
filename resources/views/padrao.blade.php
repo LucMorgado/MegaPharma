8e1565b4e35e2174e7305451e3ca4eb6c1ea7471
Lucas Morgado <lucmorgado@outlook.com>
2022-02-18 11:22:51 -0300

    first commit

---
+++ resources/views/padrao.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MegaPharma</title>
+        
+         <!-- Styles -->
+         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+
+
+    </head>
+    <body>
+        <h1>PAgina PAdrão</h1>
+    </body>
+</html>
