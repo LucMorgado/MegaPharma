78f5864bc985ffce4488aa6dc7f199cf55e94e87
Lucas Morgado <lucmorgado@outlook.com>
2022-03-03 10:25:19 -0300

    menu

---
+++ resources/views/elementos/menu.blade.php
@@ -0,0 +1,10 @@
+<nav class="menu bg-primary">
+	<ul class="container d-flex justify-content-between">
+		<li class="li text-white py-3">Nossas Lojas</li>
+		<li class="li text-white py-3">Quem Somos</li>
+		<li class="li text-white py-3">Rede Social</li>
+		<li class="li text-white py-3">Seja um Franqueado</li>
+		<li class="li text-white py-3">Depoimentos</li>
+		<li class="li text-white py-3">Contato</li>
+	</ul>
+</nav>
